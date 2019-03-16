02bf29fd2e42680a9b8b4fc419472fa478045a74
Om_G <im.omgupta@gmail.com>
2019-03-16 19:33:52 +0530

    Formatting, fixed level 1 AI so it'll actually work now.

--- t3e_ai.py
+++ t3e_ai.py
@@ -23,6 +23,8 @@ WIN_PLAYER = 13
 WIN_OPPONENT = 5
 WIN_NONE = 1
 
+################################################################################
+
 def copy_grid(major_grid):
     return deepcopy(major_grid)
 
@@ -109,20 +111,24 @@ def correct_for_owner(major_grid, start_loc, iters, player, cutoff):
     else:
         return start_loc
 
+################################################################################
 
 def scan_path(major_grid, start_loc, iters, player, cutoff):
     start_loc = correct_for_owner(major_grid, start_loc, iters, player, cutoff)
     score_grid = scan_grid(major_grid[start_loc[0]][start_loc[1]].contents, player)
     mult_grid = scan_grid(major_grid, player)
     c_cutoff = 4.5
+
     for i in range(3):
         for j in range(3):
             if major_grid[start_loc[0]][start_loc[1]].contents[i][j]:
                 score_grid[i][j] = NEG_INF
                 continue
+
             major_grid_copy = copy_grid(major_grid)
             major_grid_copy[start_loc[0]][start_loc[1]].update_cell(i, j, player)
             t3e_u.chk_grid(major_grid_copy[start_loc[0]][start_loc[1]])
+
             if not major_grid_copy[start_loc[0]][start_loc[1]].owner:
                 new_grid = scan_grid(major_grid_copy[start_loc[0]][start_loc[1]].contents,
                                      player, -1)
@@ -133,23 +139,23 @@ def scan_path(major_grid, start_loc, iters, player, cutoff):
                 winner = t3e_u.chk_grid(major_grid_copy)
                 if winner:
                     return POS_INF
+
             if iters > 0 and cutoff > time():
                 c_cutoff -= 1
                 a_cutoff = cutoff - (cutoff - time())*c_cutoff/9
                 score_grid[i][j] -= scan_path(major_grid_copy, (i, j), iters - 1,
                                               t3e_u.switch_player(player), a_cutoff)*mult_grid[i][j]/6
+
     return max(max(score_grid))
+
 
 def compute_move(major_grid, start_loc, difficulty, player, turn):
     random.seed()
 
     if difficulty == 1:
-        while major_grid[start_loc[0]][start_loc[1]].owner:
-            start_loc = (random.randrange(0, 3), random.randrange(0, 3))
-        move = (random.randrange(0, 3), random.randrange(0, 3))
-        while major_grid[start_loc[0]][start_loc[1]].contents[move[0]][move[1]]:
-            move = (random.randrange(0, 3), random.randrange(0, 3))
-        return move
+        start_loc = correct_for_owner(major_grid, start_loc, 0, player, 0)
+        score_grid = scan_grid(major_grid[start_loc[0]][start_loc[1]].contents, player)
+        return ((start_loc[0], start_loc[1]), random.choice(get_max_locs(score_grid)))
 
     elif difficulty == 2:
         iters = NORMAL_ITERS
@@ -164,18 +170,19 @@ def compute_move(major_grid, start_loc, difficulty, player, turn):
     start_loc = correct_for_owner(major_grid, start_loc, 1, player, cutoff)
     score_grid = scan_grid(major_grid[start_loc[0]][start_loc[1]].contents, player)
     mult_grid = scan_grid(major_grid, player)
-    print("Initial grid scores: " + str(score_grid))
-    print("Initial board scores: " + str(mult_grid))
     pool = Pool(processes = 9)
     results = {}
+
     for i in range(3):
         for j in range(3):
             if major_grid[start_loc[0]][start_loc[1]].contents[i][j]:
                 score_grid[i][j] = NEG_INF
                 continue
+
             major_grid_copy = copy_grid(major_grid)
             major_grid_copy[start_loc[0]][start_loc[1]].contents[i][j] = player
             t3e_u.chk_grid(major_grid_copy[start_loc[0]][start_loc[1]])
+
             if not major_grid_copy[start_loc[0]][start_loc[1]].owner:
                 new_grid = scan_grid(major_grid_copy[start_loc[0]][start_loc[1]].contents,
                                      player)
@@ -187,6 +194,7 @@ def compute_move(major_grid, start_loc, difficulty, player, turn):
                 if winner:
                     score_grid[i][j] = POS_INF
                     continue
+
             player = t3e_u.switch_player(player)
             results[(i, j)] = pool.apply_async(scan_path, args = (major_grid_copy,
                                                                   (i, j), iters,
