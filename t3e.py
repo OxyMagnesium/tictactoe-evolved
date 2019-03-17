3ccb4f88909a4a9339ad8d4a01a96d4eeb40dbae
Om_G <im.omgupta@gmail.com>
2019-03-18 01:29:54 +0530

    Changed IO system for allowing external interfacing.

--- t3e.py
+++ t3e.py
@@ -3,12 +3,52 @@
 import t3e_resources as t3e_u
 import t3e_renderer as t3e_r
 from t3e_ai import compute_move
+from threading import Thread
 from copy import deepcopy
+from time import sleep
 
 winner = None
 NEUTRAL = 20
 X = 1
 O = 5
+
+################################################################################
+
+#Output will start with one of the following with the content in double quotes:
+#INF - Simply display following content
+#REQ - Ask for input with following content
+#To access KB interrupt menu, input "KBI"
+
+def process_coords(io_h):
+    while True:
+        input = io_h.get()
+        if input == 'KBI':
+            raise KeyboardInterrupt
+        else:
+            try:
+                return (int(input.split(',')[0]), int(input.split(',')[1]))
+            except ValueError:
+                io_h.send('REQ "The values you have entered are invalid. Try again: "')
+
+class io_handler:
+    def __init__(self):
+        self.buffer = ''
+        self.wait = True
+    def send(self, intake):
+        try:
+            if intake.split('"')[0] == '' and intake.split('"')[2] == '':
+                self.buffer = 'INF ' + intake
+            else:
+                self.buffer = intake
+        except IndexError:
+            pass
+        self.wait = False
+        sleep(0.1)
+    def get(self):
+        while self.wait:
+            sleep(0.1)
+        self.wait = True
+        return self.buffer
 
 class save_state:
     def __init__(self):
@@ -26,15 +66,18 @@ class save_state:
 
 ################################################################################
 
-if __name__ == "__main__":
-    print("Welcome to TicTacToe Evolved by Om Gupta!")
-
-    ai_enabled = True if input("\nSelect player count (1/2): ") == '1' else False
+def main(io_h, players):
+    if not players:
+        io_h.send('REQ "Select player count (1/2): "')
+        players = int(io_h.get())
+    ai_enabled = True if players == 1 else False
     if ai_enabled:
-        ai_player = input("Select your type (X/O, X goes first): ")
+        io_h.send('REQ "Select your type (X/O; X goes first): "')
+        ai_player = io_h.get()
         ai_player = O if ai_player == 'X' else X
-        print("Select difficulty level:\n1. Easy\n2. Normal\n3. Hard\n4. Legendary")
-        difficulty = int(input("Enter your choice (1/2/3/4): "))
+        io_h.send('INF "Select difficulty level:\n1. Easy\n2. Normal\n3. Hard\n4. Legendary"')
+        io_h.send('REQ "Enter your choice (1/2/3/4): "')
+        difficulty = int(io_h.get())
 
     turn_m0 = save_state()
     turn_m1 = save_state()
@@ -44,78 +87,83 @@ if __name__ == "__main__":
     r_coord = 1
     c_coord = 1
     turn = 1
-    
-    print("\nStarting game.")
+
+    io_h.send('INF "\nStarting game."')
 
     while True:
         try:
-            print("\n################################################################################\n{0}'s TURN."
-                  .format(t3e_u.displayer(player)))
-            print(t3e_r.render(major_grid))
+            io_h.send('"\n################################################################################\n"')
+            io_h.send('"{0}\'s TURN."'.format(t3e_u.displayer(player)))
+            io_h.send('"{0}"'.format(t3e_r.render(major_grid)))
             focus_grid = major_grid[r_coord][c_coord]
 
             if not ai_enabled or player != ai_player:
                 if focus_grid.owner:
-                    print("\nThe grid you landed on is already owned. Select a new grid to move in.")
+                    io_h.send('"\nThe grid you landed on is already owned. Select a new grid to move in."')
                     while True:
                         try:
-                            tr_coord = int(input("Enter row coordinate of new grid: ")) - 1
-                            tc_coord = int(input("Enter col coordinate of new grid: ")) - 1
+                            io_h.send('REQ "Enter coordinates of new grid (R, C): "')
+                            loc = process_coords(io_h)
+                            tr_coord = loc[0] - 1
+                            tc_coord = loc[1] - 1
                             if not major_grid[tr_coord][tc_coord].owner:
                                 focus_grid = major_grid[tr_coord][tc_coord]
                                 break
                             else:
-                                print("\nInvalid coordinates.\n")
-                        except Exception:
-                            print("\nInvalid coordinates.\n")
+                                io_h.send('"\nInvalid coordinates.\n"')
+                        except IndexError:
+                            io_h.send('"\nInvalid coordinates.\n"')
 
-                print("\nYou are moving in grid ({0}, {1})."
-                      .format(str(focus_grid.location[0] + 1),
-                              str(focus_grid.location[1] + 1)))
+                io_h.send('"\nYou are moving in grid ({0}, {1})."'
+                          .format(str(focus_grid.location[0] + 1),
+                                  str(focus_grid.location[1] + 1)))
                 while True:
                     try:
-                        tr_coord = int(input("Enter row coordinate of your move: ")) - 1
-                        tc_coord = int(input("Enter col coordinate of your move: ")) - 1
+                        io_h.send('REQ "Enter coordinates of your move (R, C): "')
+                        loc = process_coords(io_h)
+                        tr_coord = loc[0] - 1
+                        tc_coord = loc[1] - 1
                         if focus_grid.contents[tr_coord][tc_coord] == 0:
                             r_coord = tr_coord
                             c_coord = tc_coord
                             focus_grid.update_cell(r_coord, c_coord, player)
                             break
                         else:
-                            print("\nInvalid coordinates.\n")
-                    except Exception:
-                        print("\nInvalid coordinates.\n")
+                            io_h.send('"\nInvalid coordinates.\n"')
+                    except IndexError:
+                        io_h.send('"\nInvalid coordinates.\n"')
             else:
-                print("\nWaiting for computer to move.")
+                io_h.send('"\nWaiting for computer to move."')
                 ai_move = compute_move(major_grid, focus_grid.location, difficulty,
                                        player, turn)
                 focus_grid = major_grid[ai_move[0][0]][ai_move[0][1]]
                 r_coord = ai_move[1][0]
                 c_coord = ai_move[1][1]
                 focus_grid.update_cell(r_coord, c_coord, player)
-                ai_move = [[elem + 1 for elem in pair] for pair in ai_move]
-                print("{0} moved in grid {1}, cell {2}.".format(t3e_u.displayer(player),
-                                                                ai_move[0], ai_move[1]))
+                loc = 'grid ({0}, {1}), cell ({2}, {3})'.format(ai_move[0][0] + 1,
+                                                                ai_move[0][1] + 1,
+                                                                ai_move[1][0] + 1,
+                                                                ai_move[1][1] + 1,)
+                io_h.send('"{0} moved in {1}."'.format(t3e_u.displayer(player), loc))
 
             t3e_u.chk_grid(focus_grid)
             if focus_grid.owner and focus_grid.owner != NEUTRAL:
-                print("\n{0} has won grid ({1}, {2})!"
-                      .format(t3e_u.displayer(focus_grid.owner),
-                              str(focus_grid.location[0] + 1),
-                              str(focus_grid.location[1] + 1)))
+                io_h.send('"\n{0} has won grid ({1}, {2})!"'
+                          .format(t3e_u.displayer(focus_grid.owner),
+                                  str(focus_grid.location[0] + 1),
+                                  str(focus_grid.location[1] + 1)))
                 winner = t3e_u.chk_grid(major_grid)
                 if winner:
-                    print("\n################################################################################\n"
-                          .format(t3e_u.displayer(player)))
-                    print(t3e_r.render(major_grid))
+                    io_h.send('"\n################################################################################\n"')
+                    io_h.send(t3e_r.render(major_grid))
                     break
             elif focus_grid.owner == NEUTRAL:
-                print("\nGrid ({0}, {1}) is drawn - no winner possible."
-                      .format(str(focus_grid.location[0] + 1),
-                              str(focus_grid.location[1] + 1)))
+                io_h.send('"\nGrid ({0}, {1}) is drawn - no winner possible."'
+                          .format(str(focus_grid.location[0] + 1),
+                                  str(focus_grid.location[1] + 1)))
                 t3e_u.chk_grid(major_grid)
                 if winner == NEUTRAL:
-                    print(t3e_r.render(major_grid))
+                    io_h.send(t3e_r.render(major_grid))
                     break
             player = t3e_u.switch_player(player)
             turn += 1
@@ -125,11 +173,12 @@ if __name__ == "__main__":
             turn_m0.save(major_grid, player, r_coord, c_coord, False)
 
         except KeyboardInterrupt:
-            print("\n\nKeyboard interrupt menu:\n1. Revert turn\n2. Exit game\n3. Cancel")
-            choice = int(input("Enter your choice (1/2/3): "))
+            io_h.send('INF "\n\nMenu:\n1. Revert turn\n2. Exit game\n3. Cancel"')
+            io_h.send('REQ "Enter your choice (1/2/3): "')
+            choice = int(io_h.get())
             if choice == 1:
                 if turn_m2.loaded:
-                    print("Cannot revert right now. Resuming game.")
+                    io_h.send('"\nCannot revert right now. Resuming game."')
                 else:
                     major_grid = deepcopy(turn_m2.grid)
                     player = turn_m2.player
@@ -138,18 +187,44 @@ if __name__ == "__main__":
                     turn_m0.loaded = True
                     turn_m1.loaded = True
                     turn_m2.loaded = True
-                    print("Continuing from previous turn.")
+                    io_h.send('"\nContinuing from previous turn."')
             elif choice == 2:
-                pause = input("Game ended. Press enter to exit.")
+                io_h.send('"\nGame ended."')
+                io_h.send('END')
                 exit()
             elif choice == 3:
-                print("Resuming game.")
+                io_h.send('"\nResuming game."')
             else:
-                print("Invalid choice. Resuming game.")
+                io_h.send('"\nInvalid choice. Resuming game."')
 
     if winner != NEUTRAL:
-        print("\n\n{0} has won the game!".format(t3e_u.displayer(winner)))
+        io_h.send('"\n\n{0} has won the game!"'.format(t3e_u.displayer(winner)))
     else:
-        print("Game is drawn!")
-    pause = input("Press enter to exit.")
+        io_h.send('"Game is drawn!"')
+    io_h.send('END')
     exit()
+
+def play():
+    io_h = io_handler()
+    game_thread = Thread(target = main, args = (io_h, None))
+    game_thread.start()
+
+    while game_thread.is_alive():
+        intake = io_h.get().split('"')
+        if intake[0].strip() == 'INF':
+            print(intake[1])
+        elif intake[0].strip() == 'REQ':
+            try:
+                io_h.send(input(intake[1]))
+            except KeyboardInterrupt:
+                io_h.send('KBI')
+        elif intake[0].strip() == 'END':
+            break
+        else:
+            print("An internal error occured.")
+
+    input("Press enter to exit. ")
+    exit()
+
+if __name__ == '__main__':
+    play()
