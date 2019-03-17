61d8023ee2ab4179573d67e7ab4f4da9cf0f5fec
Om_G <im.omgupta@gmail.com>
2019-03-17 15:41:26 +0530

    Fixed and improved revert functionality.

--- t3e.py
+++ t3e.py
@@ -3,48 +3,26 @@
 import t3e_resources as t3e_u
 import t3e_renderer as t3e_r
 from t3e_ai import compute_move
+from copy import deepcopy
 
 winner = None
 NEUTRAL = 20
 X = 1
 O = 5
 
-major_grid = [[t3e_u.sub_grid(a, b) for b in range(3)] for a in range(3)]
-
 class save_state:
     def __init__(self):
+        self.grid = []
+        self.player = 0
+        self.r = 0
+        self.c = 0
         self.loaded = True
-        self.l_r_coord = 1
-        self.l_c_coord = 1
-    def save(self, e_r_coord, e_c_coord):
-        self.loaded = False
-        self.l_r_coord = e_r_coord
-        self.l_c_coord = e_c_coord
-    def load(self):
-        if self.loaded:
-            print("Can't revert twice. Resuming game.")
-            return
-        global major_grid
-        global r_coord
-        global c_coord
-        global turn
-        nl_r_coord = front_save.l_r_coord
-        nl_c_coord = front_save.l_c_coord
-        l_r_coord = self.l_r_coord
-        l_c_coord = self.l_c_coord
-        major_grid[nl_r_coord][nl_c_coord].contents[r_coord][c_coord] = 0
-        major_grid[l_r_coord][l_c_coord].contents[nl_r_coord][nl_c_coord] = 0
-        r_coord = l_r_coord
-        c_coord = l_c_coord
-        turn -= 2
-        self.loaded = True
-
-turn = 1
-r_coord = 1
-c_coord = 1
-front_save = save_state()
-back_save = save_state()
-save = save_state()
+    def save(self, major_grid, player, r_coord, c_coord, loaded):
+        self.grid = deepcopy(major_grid)
+        self.player = player
+        self.r = r_coord
+        self.c = c_coord
+        self.loaded = loaded
 
 ################################################################################
 
@@ -58,7 +36,15 @@ if __name__ == "__main__":
         print("Select difficulty level:\n1. Easy\n2. Normal\n3. Hard\n4. Legendary")
         difficulty = int(input("Enter your choice (1/2/3/4): "))
 
+    turn_m0 = save_state()
+    turn_m1 = save_state()
+    turn_m2 = save_state()
+    major_grid = [[t3e_u.sub_grid(a, b) for b in range(3)] for a in range(3)]
     player = X
+    r_coord = 1
+    c_coord = 1
+    turn = 1
+    
     print("\nStarting game.")
 
     while True:
@@ -100,13 +86,16 @@ if __name__ == "__main__":
                     except Exception:
                         print("\nInvalid coordinates.\n")
             else:
-                print("\nAI's move.")
+                print("\nWaiting for computer to move.")
                 ai_move = compute_move(major_grid, focus_grid.location, difficulty,
                                        player, turn)
                 focus_grid = major_grid[ai_move[0][0]][ai_move[0][1]]
                 r_coord = ai_move[1][0]
                 c_coord = ai_move[1][1]
                 focus_grid.update_cell(r_coord, c_coord, player)
+                ai_move = [[elem + 1 for elem in pair] for pair in ai_move]
+                print("{0} moved in grid {1}, cell {2}.".format(t3e_u.displayer(player),
+                                                                ai_move[0], ai_move[1]))
 
             t3e_u.chk_grid(focus_grid)
             if focus_grid.owner and focus_grid.owner != NEUTRAL:
@@ -131,13 +120,25 @@ if __name__ == "__main__":
             player = t3e_u.switch_player(player)
             turn += 1
 
+            turn_m2.save(turn_m1.grid, turn_m1.player, turn_m1.r, turn_m1.c, turn_m1.loaded)
+            turn_m1.save(turn_m0.grid, turn_m0.player, turn_m0.r, turn_m0.c, turn_m0.loaded)
+            turn_m0.save(major_grid, player, r_coord, c_coord, False)
+
         except KeyboardInterrupt:
             print("\n\nKeyboard interrupt menu:\n1. Revert turn\n2. Exit game\n3. Cancel")
             choice = int(input("Enter your choice (1/2/3): "))
             if choice == 1:
-                print("Continuing from previous turn.")
-                save.load()
-                continue
+                if turn_m2.loaded:
+                    print("Cannot revert right now. Resuming game.")
+                else:
+                    major_grid = deepcopy(turn_m2.grid)
+                    player = turn_m2.player
+                    r_coord = turn_m2.r
+                    c_coord = turn_m2.c
+                    turn_m0.loaded = True
+                    turn_m1.loaded = True
+                    turn_m2.loaded = True
+                    print("Continuing from previous turn.")
             elif choice == 2:
                 pause = input("Game ended. Press enter to exit.")
                 exit()
@@ -145,10 +146,6 @@ if __name__ == "__main__":
                 print("Resuming game.")
             else:
                 print("Invalid choice. Resuming game.")
-
-        save.save(back_save.l_r_coord, back_save.l_c_coord)
-        back_save.save(front_save.l_r_coord, front_save.l_c_coord)
-        front_save.save(r_coord, c_coord)
 
     if winner != NEUTRAL:
         print("\n\n{0} has won the game!".format(t3e_u.displayer(winner)))
